f7feb95cd4b9014fb649465ae79b5c80d569a1cf
TomiTsuma <tommytsuma7@gmail.com>
2025-04-25 18:45:47 +0300

    Added script to register docker image to ecr

---
+++ scripts/build_and_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -a 
+source ../.env
+set +a 
+
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+aws ecr describe-repositories --repository-names ${ECR_REPOSITORY} ||  aws ecr create-repository --repository-name ${ECR_REPOSITORY}
+
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+docker build -t ${ECR_REPOSITORY}:${IMAGE_TAG} .
+
+docker tag ${ECR_REPOSITORY}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${IMAGE_TAG}
+
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}:${IMAGE_TAG}
+
+echo "ECR Repository URI: ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPOSITORY}" 
